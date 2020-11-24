85878896482e4fa9ed889212bfd918236e83c9c7
manurajp83 <74589077+manurajp83@users.noreply.github.com>
2020-11-24 08:27:04 -0800

    Update square.py

--- square.py
+++ square.py
@@ -1,5 +1,5 @@
 #PROGRAM TO FIND THE AREA OF A SQUARE
                                             
-c=int(input("Enter a number:")) #to enter the side
-square=(c*c)                                  #to calculate area
-print("square is:",square)                     #to display  area
+c=int(input("Enter a side:")) #to enter the side
+square=(c*c)                       #to calculate area
+print("area of square is:",square)     #to display  area
