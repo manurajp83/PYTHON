2e24d5bba81de03106c40c59ba35bf867b83a107
manurajp83 <74589077+manurajp83@users.noreply.github.com>
2020-11-24 08:21:23 -0800

    Update SI.py

--- SI.py
+++ SI.py
@@ -4,3 +4,10 @@ p=int(input("Enter the amount:"))#to enter the amount
 r=int(input("Enter the rate:"))   #to enter the rate
 t=int(input("Enter no of years:"))#to enter the time
 print("Simple interest is:",(p*r*t)/100) #displays the SI
+
+'''output
+Enter the amount:3000
+Enter the rate:30
+Enter no of years:5
+Simple interest is: 4500.0
+'''
