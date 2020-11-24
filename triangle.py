27de449fe40f2a5ed98ec160a1e31cbe86e39b75
manurajp83 <74589077+manurajp83@users.noreply.github.com>
2020-11-24 08:28:20 -0800

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -2,3 +2,9 @@
 l=int(input("Enter the length:"))                 #to enter the length
 b=int(input("Enter the breadth:"))                #to enter the breadth           
 print("The area of right angled triangle is:",(l*b)/2)#displays the area
+
+'''output
+Enter the length:5
+Enter the breadth:6
+The area of right angled triangle is: 15.0
+'''
