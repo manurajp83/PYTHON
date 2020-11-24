bd1052bc1aa66fc0a18fa80fdebf632b73fef341
manurajp83 <74589077+manurajp83@users.noreply.github.com>
2020-11-24 08:25:56 -0800

    Update number.py

--- number.py
+++ number.py
@@ -2,3 +2,9 @@
 a=int(input("Enter a no.:")) #to enter the number
 print("Previous no. is:",a-1) #displays previous no.
 print("Next no. is :",a+1)    #displays next no.
+
+'''output
+Enter a no.:5
+Previous no. is: 4
+Next no. is : 6
+'''
