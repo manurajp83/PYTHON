9307c8461463bd4d3b6b6d090f0a681fdf3ff056
manurajp83 <manurajp1997@gmail.com>
2020-12-03 04:26:32 -0800

    2nd commit

---
+++ exercise 2/circle.py
@@ -0,0 +1,12 @@
+#program to find area of circle
+
+import math
+r=int(input("enter the radius")) 
+area=math.pi*r*r
+print("area is %.2f"%area)
+
+'''output
+enter the radius 8
+area is 201.06
+'''
+
