b5041737aa77dc593c807a785302d36db9aa4d57
manurajp83 <manurajp1997@gmail.com>
2020-12-07 08:57:18 -0800

    commit

--- exercise 4/dicrminent.py
+++
@@ -1,9 +0,0 @@
-a,b,c=int(input("enter value of a : ")),int(input("enter value of b : ")),int(input("enter value of c : "))
-print("Discriminant = ",(b**2)-(4*a*c))
-
-'''output
-enter value of a : 1
-enter value of b : 4
-enter value of c : 2
-Discriminant =  8
-'''
