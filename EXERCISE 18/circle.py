756279fb64ef0dd2a86701a55da768a86fe5e330
manurajp83 <manurajp1997@gmail.com>
2021-02-11 10:34:43 +0530

    commit

---
+++ EXERCISE 18/circle.py
@@ -0,0 +1,9 @@
+#MODULE CIRCLE
+import math
+# Circle area function
+def area(radius):
+    return (math.pi*radius**2) 
+# Circle perimeter function
+def perimeter(radius):
+    return (math.pi*2*radius)
+
---
+++ EXERCISE 18/rectangle.py
@@ -0,0 +1,9 @@
+          # MODULE RECTANGLE
+   
+  #area function
+def area(width,length):
+    return width*length
+  
+    #perimeter function
+def perimeter(length,width):
+    return (2*(length + width))
